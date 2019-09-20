6818b3be4466bb34f621f6d63fd6c0ab4c6033f4
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-09-19 22:34:23 -0500

    Added LandingPage CSS

--- template/template.php
+++ template/template.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php page_title(); ?> | <?php site_name(); ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="landing-page.css" href="/template/landing-page.css">
     <link href="/template/master.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
