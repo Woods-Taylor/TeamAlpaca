3c3bfa54646a625904c85c0436f48b2a3d281e0b
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-10-08 17:20:38 -0500

    testing navmenu() changes

--- includes/functions.php
+++ includes/functions.php
@@ -34,7 +34,7 @@ function nav_menu($sep = ' | ')
     foreach ($nav_items as $uri => $name) {
         $class = str_replace('page=', '', $_SERVER['QUERY_STRING']) == $uri ? ' active' : '';
         $url = config('site_url') . '/' . (config('pretty_uri') || $uri == '' ? '' : '?page=') . $uri;
-        $nav_menu .= '<a href="' . $url . '" title="' . $name . '" class="item ' . $class . '">' . $name . '</a>' . $sep;
+        $nav_menu .= '<li class="nav-item active">' '<a href="' . $url . '" title="' . $name . '" class="item ' . $class . '">' . $name . '</a>' . $sep '</li>';
     }
     echo trim($nav_menu, $sep);
 }
